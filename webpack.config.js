86ddc7c7d2c44e911a349e4b217c05c121893f47
Si Wan Kim <siwankim@utexas.edu>
2017-08-23 21:03:50 -0700

    house favicon

--- webpack.config.js
+++ webpack.config.js
@@ -49,7 +49,7 @@ const path = require('path');
 
 module.exports = {
   entry: [
-    'webpack-dev-server/client?http://localhost:5000',
+    'webpack-dev-server/client?http://localhost:3000',
     'webpack/hot/dev-server',
     './scripts/index'
   ],
